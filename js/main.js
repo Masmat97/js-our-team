54ec605ef6950fb0dedceab4431949c59273052b
Matteo Mascio <matteo.mascio@libero.it>
2024-05-06 16:03:44 +0200

    M1

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-
+// Creazione array utente1
 const utente1 = {
     nome: "Wayne Barnett",
     ruolo: "Founder & CEO", 
     immagine: "wayne-barnett-founder-ceo.jpg",
 }
-console.log(utente1)
-
+console.log( "Nome/ruolo: ", `${utente1.nome} ${utente1.ruolo}` );
+// Creazione array utente2
 const utente2 = {
     nome: "Angela Caroll",
     ruolo: "Chief Editor",
     immagine: "angela-caroll-chief-editor.jpg",
 }
-console.log(utente2)
-
+console.log( "Nome/ruolo: ", `${utente2.nome} ${utente2.ruolo}` );
+// Creazione array utente3
 const utente3 = {
     nome: "Walter Gordon",
     ruolo: "Office Manager",
     immagine: "walter-gordon-office-manager.jpg",
 }
-console.log(utente3)
-
+console.log( "Nome/ruolo: ", `${utente3.nome} ${utente3.ruolo}` );
+// Creazione array utente4
 const utente4 = {
     nome: "Angela Lopez",
     ruolo: "Social Media Manager",
     immagine: "angela-lopez-social-media-manager.jpg",
 }
-console.log(utente4)
-
+console.log( "Nome/ruolo: ", `${utente4.nome} ${utente4.ruolo}` );
+// Creazione array utente5
 const utente5 = {
     nome: "Scott Estrada",
     ruolo: "Developer",
     immagine: "scott-estrada-developer.jpg",
 }
-console.log(utente5)
-
+console.log( "Nome/ruolo: ", `${utente5.nome} ${utente5.ruolo}` );
+// Creazione array utente6
 const utente6 = {
     nome: "Barbara Ramos",
     ruolo: "Graphic Designer",
     immagine: "barbara-ramos-graphic-designer.jpg",
 }
-console.log(utente6)
+console.log( "Nome/ruolo: ", `${utente6.nome} ${utente6.ruolo}` );
