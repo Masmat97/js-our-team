4b7ca81067002db1ebb039b395fb91ba33fb95bf
Matteo Mascio <matteo.mascio@libero.it>
2024-05-06 15:58:49 +0200

    M0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
-const utente = {
-    nome: Wayne Barnett,
-    ruolo: Founder & CEO,
-    immagine: wayne-barnett-founder-ceo.jpg,
+
+const utente1 = {
+    nome: "Wayne Barnett",
+    ruolo: "Founder & CEO", 
+    immagine: "wayne-barnett-founder-ceo.jpg",
 }
+console.log(utente1)
+
+const utente2 = {
+    nome: "Angela Caroll",
+    ruolo: "Chief Editor",
+    immagine: "angela-caroll-chief-editor.jpg",
+}
+console.log(utente2)
+
+const utente3 = {
+    nome: "Walter Gordon",
+    ruolo: "Office Manager",
+    immagine: "walter-gordon-office-manager.jpg",
+}
+console.log(utente3)
+
+const utente4 = {
+    nome: "Angela Lopez",
+    ruolo: "Social Media Manager",
+    immagine: "angela-lopez-social-media-manager.jpg",
+}
+console.log(utente4)
+
+const utente5 = {
+    nome: "Scott Estrada",
+    ruolo: "Developer",
+    immagine: "scott-estrada-developer.jpg",
+}
+console.log(utente5)
+
+const utente6 = {
+    nome: "Barbara Ramos",
+    ruolo: "Graphic Designer",
+    immagine: "barbara-ramos-graphic-designer.jpg",
+}
+console.log(utente6)
