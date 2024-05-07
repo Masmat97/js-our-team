a878159d6c6fa0ddb7a6d3e6ee3a94880f411193
Matteo Mascio <matteo.mascio@libero.it>
2024-05-07 11:33:32 +0200

    Bonus1

--- js/main.js
+++ js/main.js
@@ -1,29 +1,24 @@
 
-let utenti = [
+let team = [
     { nome: "Wayne Barnett", ruolo: "Founder & CEO", immagine: "wayne-barnett-founder-ceo.jpg", },
     { nome: "Angela Caroll", ruolo: "Chief Editor", immagine: "angela-caroll-chief-editor.jpg", },
     { nome: "Walter Gordon", ruolo: "Office Manager", immagine: "walter-gordon-office-manager.jpg", },
     { nome: "Angela Lopez", ruolo: "Social Media Manager", immagine: "angela-lopez-social-media-manager.jpg", },
     { nome: "Scott Estrada", ruolo: "Developer", immagine: "scott-estrada-developer.jpg", },
-    { nome: "Barbara Ramos", ruolo: "Graphic Designer", immagine: "barbara-ramos-graphic-designer.jpg",}
+    { nome: "Barbara Ramos", ruolo: "Graphic Designer", immagine: "barbara-ramos-graphic-designer.jpg", }
 
-    
+
 ]
-console.log(utenti)
+console.log(team)
 
+const container = document.getElementById("container");
 
+for (let i = 0; i < team.length; i++) {
+    let membro = team[i]
 
-// Stampare su console il nome e il ruolo di ogni membro del team
-utenti.forEach((membro) => {
-    console.log("Nome:", membro.nome, "| Ruolo:", membro.ruolo);
-  });
-  
-  // Trova un elemento HTML dove vuoi stampare i dettagli del team
-  const container = document.getElementById("container");
-  
-  // Stampare in pagina nome, ruolo e immagine per ogni membro del team
-  utenti.forEach((membro) => {
-    const elemento = document.createElement("div");
-    elemento.textContent = `Nome: ${membro.nome}, Ruolo: ${membro.ruolo}, Immagine: ${membro.immagine}`;
-    container.appendChild(elemento);
-  });
+    container.innerHTML += `<div class="card">
+                            <img src="./img/${membro.immagine}"></img>
+                            <p>Nome: ${membro.nome}</p>
+                            <p>Ruolo: ${membro.ruolo}</p>
+                            </div>`
+}
