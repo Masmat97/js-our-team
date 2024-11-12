372725dadecbdddbfa7ff7467e7ed1a1a9d32dd0
Masmat97 <matteo.mascio@libero.it>
2024-11-12 17:20:18 +0100

    fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-let team = [
+let teamMembers = [
     { nome: "Wayne Barnett", ruolo: "Founder & CEO", immagine: "wayne-barnett-founder-ceo.jpg", },
     { nome: "Angela Caroll", ruolo: "Chief Editor", immagine: "angela-caroll-chief-editor.jpg", },
     { nome: "Walter Gordon", ruolo: "Office Manager", immagine: "walter-gordon-office-manager.jpg", },
@@ -9,16 +9,37 @@ let team = [
 
 
 ]
-console.log(team)
+console.log(teamMembers)
 
-const container = document.getElementById("container");
+// const container = document.getElementById("container");
 
-for (let i = 0; i < team.length; i++) {
-    let membro = team[i]
+// for (let i = 0; i < team.length; i++) {
+//     let membro = team[i]
 
-    container.innerHTML += `<div class="card">
-                            <img src="./img/${membro.immagine}"></img>
-                            <p>Nome: ${membro.nome}</p>
-                            <p>Ruolo: ${membro.ruolo}</p>
-                            </div>`
+//     container.innerHTML += `<div class="card">
+//                             <img src="./img/${membro.immagine}"></img>
+//                             <p>Nome: ${membro.nome}</p>
+//                             <p>Ruolo: ${membro.ruolo}</p>
+//                             </div>`
+// }
+
+teamMembers.forEach(member => {
+    console.log(`Nome: ${member.nome}, Ruolo: ${member.ruolo}`);
+});
+
+// Funzione per creare l'HTML per ogni membro
+function creaCard(member) {
+    return `
+        <div class="card">
+            <h2>${member.nome}</h2>
+            <p>Ruolo: ${member.ruolo}</p>
+            <img src="./img/${member.immagine}"></img>
+        </div>
+    `;
 }
+
+// MILESTONE 2: Stampa in pagina
+const teamContainer = document.getElementById('team-container');
+teamMembers.forEach(member => {
+    teamContainer.innerHTML += creaCard(member);
+});
--- readme.md
+++ readme.md
@@ -1,4 +1,14 @@
-JS-OUR-TEAM:
--creare l'arrey di oggetti;
--stampare su console;
-_stampare in pagina;
+CONSEGNA
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite (in allegato zip).
+MILESTONE 1:
+Stampare su console, per ogni membro del team: il nome e il ruolo.
+MILESTONE 2:
+Stampare in pagina, per ogni membro del team: nome, ruolo e immagine, come semplici stringhe.
+Svolgere questa milestone grazie a una funzione può semplificarvi il lavoro per i bonus. La funzione riceve un oggetto e restituisce una stringa di HTML 
+BONUS 1:
+Trasformare il testo col percorso della foto in una immagine effettiva (in allegato txt).
+BONUS 2:
+Organizzare i singoli membri in card/schede.
